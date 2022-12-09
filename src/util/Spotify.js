e6c1a9382ba42364e14267e8787a2c29daca1019
MiyanoX <MiyanoX@users.noreply.github.com>
2022-12-10 03:33:02 +0900

    Obtain Spotify Access Token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,11 +1,24 @@
-let userAccessToken;
+let accessToken;
+let clientID = 'd866cbcf85a742daac8ae5eb0aea9d0a';
+let redirectURI = "http://localhost:3000/";
 
 let Spotify = {
     getAccessToken() {
-        if (!userAccessToken) {
-            return userAccessToken;
+        if (!accessToken) {
+            return accessToken;
         }
-    },
+        const URL = window.location.href;
+        if (!URL.match(/error=([^&]*)/)) {
+            accessToken = URL.match(/access_token=([^&]*)/);
+            const expireIn= URL.match(/expires_in=([^&])*/);
+            window.setTimeout(() => accessToken = '', expireIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+        } else {
+            window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+        }
+
+        
+    }
     
 
 };
